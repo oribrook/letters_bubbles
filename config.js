c903172df499108ede6ea0cbcf5737a15333383f
ori_b <ori@ori>
2024-02-13 10:55:44 +0200

    update

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-BUBBLE_SIZE = 50;
+BUBBLE_SIZE = 100;
 maxLetters = 4;
 gameSpeed = 20;
 
 const canvasElem = document.getElementById("canvas");
 const ctx = canvasElem.getContext("2d");
-CANVAS_WIDTH = canvasElem.width = 800;
-CANVAS_HEIGHT = canvasElem.height = 800;
+CANVAS_WIDTH = canvasElem.width = window.innerWidth * 0.9;
+CANVAS_HEIGHT = canvasElem.height = window.innerHeight * 0.7;
 
 mainCanvas = document.getElementById("canvas");
 rect = mainCanvas.getBoundingClientRect();
